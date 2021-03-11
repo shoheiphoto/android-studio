ceaf54b7907443f04019f0b546c64be2ab4c9c8b
shohei <sh12x29@gmail.com>
2021-03-11 16:26:17 +0900

    androidアプリ練習

---
+++ AndroidStudioProjects/practice_button_move/app/src/main/java/com/example/practice_button_move/MainActivity.java
@@ -0,0 +1,228 @@
+package com.example.practice_button_move;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button)findViewById(R.id.ID_BTN1);
+        button1.setOnClickListener(new Button1ClickListener());;
+        button1.setOnLongClickListener(new Button1LongClickListener());
+
+        Button button2 = (Button)findViewById(R.id.ID_BTN2);
+        button2.setOnClickListener(new Button2ClickListener());;
+        button2.setOnLongClickListener(new Button2LongClickListener());
+
+        Button button3 = (Button)findViewById(R.id.ID_BTN3);
+        button3.setOnClickListener(new Button3ClickListener());;
+        button3.setOnLongClickListener(new Button3LongClickListener());
+
+        Button button4 = (Button)findViewById(R.id.ID_BTN4);
+        button4.setOnClickListener(new Button4ClickListener());;
+        button4.setOnLongClickListener(new Button4LongClickListener());
+
+        Button button5 = (Button)findViewById(R.id.ID_BTN5);
+        button5.setOnClickListener(new Button5ClickListener());;
+//        button4.setOnLongClickListener(new Button4LongClickListener());
+
+
+
+    }
+
+    class Button1ClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            TextView txtview = (TextView) findViewById(R.id.ID_TXTVIEW);
+            ViewGroup.LayoutParams lp = txtview.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams)lp;
+            txtview.setText("左？");
+            mlp.setMargins(mlp.leftMargin - 40, mlp.topMargin, mlp.rightMargin + 40, mlp.bottomMargin);
+            txtview.setLayoutParams(mlp);
+
+            Random rand = new Random();
+            int num1 = rand.nextInt(60) - 120;
+            int num2 = rand.nextInt(60) - 120;
+            int num3 = rand.nextInt(60) - 120;
+            int num4 = rand.nextInt(60) - 120;
+            TextView txtview2 = (TextView) findViewById(R.id.Button01);
+            ViewGroup.LayoutParams lp2 = txtview2.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp2 = (ViewGroup.MarginLayoutParams)lp2;
+            txtview2.setText("適当に動いときます");
+            mlp2.setMargins(mlp2.leftMargin + num1, mlp2.topMargin + num2, mlp2.rightMargin + num3, mlp2.bottomMargin + num4);
+            txtview2.setLayoutParams(mlp2);
+        }
+    }
+
+    class Button2ClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            TextView txtview = (TextView)findViewById(R.id.ID_TXTVIEW);
+            ViewGroup.LayoutParams lp = txtview.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams)lp;
+            txtview.setText("右？");
+            mlp.setMargins(mlp.leftMargin + 40, mlp.topMargin, mlp.rightMargin - 40, mlp.bottomMargin);
+            txtview.setLayoutParams(mlp);
+
+
+            Random rand = new Random();
+            int num1 = rand.nextInt(60) - 120;
+            int num2 = rand.nextInt(60) - 120;
+            int num3 = rand.nextInt(60) - 120;
+            int num4 = rand.nextInt(60) - 120;
+            TextView txtview2 = (TextView) findViewById(R.id.Button01);
+            ViewGroup.LayoutParams lp2 = txtview2.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp2 = (ViewGroup.MarginLayoutParams)lp2;
+            txtview2.setText("適当に動いときます");
+            mlp2.setMargins(mlp2.leftMargin + num1, mlp2.topMargin + num2, mlp2.rightMargin + num3, mlp2.bottomMargin + num4);
+            txtview2.setLayoutParams(mlp2);
+        }
+    }
+
+    class Button3ClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            TextView txtview = (TextView) findViewById(R.id.ID_TXTVIEW);
+            ViewGroup.LayoutParams lp = txtview.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams)lp;
+            txtview.setText("上？");
+            mlp.setMargins(mlp.leftMargin, mlp.topMargin - 40, mlp.rightMargin, mlp.bottomMargin + 40);
+            txtview.setLayoutParams(mlp);
+
+
+            Random rand = new Random();
+            int num1 = rand.nextInt(60) - 120;
+            int num2 = rand.nextInt(60) - 120;
+            int num3 = rand.nextInt(60) - 120;
+            int num4 = rand.nextInt(60) - 120;
+            TextView txtview2 = (TextView) findViewById(R.id.Button01);
+            ViewGroup.LayoutParams lp2 = txtview2.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp2 = (ViewGroup.MarginLayoutParams)lp2;
+            txtview2.setText("適当に動いときます");
+            mlp2.setMargins(mlp2.leftMargin + num1, mlp2.topMargin + num2, mlp2.rightMargin + num3, mlp2.bottomMargin + num4);
+            txtview2.setLayoutParams(mlp2);
+        }
+    }
+
+    class Button4ClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            TextView txtview = (TextView)findViewById(R.id.ID_TXTVIEW);
+            ViewGroup.LayoutParams lp = txtview.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams)lp;
+            txtview.setText("下？");
+            mlp.setMargins(mlp.leftMargin, mlp.topMargin + 40, mlp.rightMargin, mlp.bottomMargin - 40);
+            txtview.setLayoutParams(mlp);
+
+
+            Random rand = new Random();
+            int num1 = rand.nextInt(60) - 120;
+            int num2 = rand.nextInt(60) - 120;
+            int num3 = rand.nextInt(60) - 120;
+            int num4 = rand.nextInt(60) - 120;
+            TextView txtview2 = (TextView) findViewById(R.id.Button01);
+            ViewGroup.LayoutParams lp2 = txtview2.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp2 = (ViewGroup.MarginLayoutParams)lp2;
+            txtview2.setText("適当に動いときます");
+            mlp2.setMargins(mlp2.leftMargin + num1, mlp2.topMargin + num2, mlp2.rightMargin + num3, mlp2.bottomMargin + num4);
+            txtview2.setLayoutParams(mlp2);
+        }
+    }
+
+
+    class Button5ClickListener implements View.OnClickListener {
+        public void onClick(View v) {
+            TextView txtview = (TextView) findViewById(R.id.ID_TXTVIEW);
+            ViewGroup.LayoutParams lp = txtview.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams)lp;
+            txtview.setText("かえりました");
+            mlp.setMargins(mlp.leftMargin = 0, mlp.topMargin = 0, mlp.rightMargin = 0, mlp.bottomMargin = 0);
+            txtview.setLayoutParams(mlp);
+
+
+
+            Random rand = new Random();
+            int num1 = rand.nextInt(60) - 120;
+            int num2 = rand.nextInt(60) - 120;
+            int num3 = rand.nextInt(60) - 120;
+            int num4 = rand.nextInt(60) - 120;
+            TextView txtview2 = (TextView) findViewById(R.id.Button01);
+            ViewGroup.LayoutParams lp2 = txtview2.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp2 = (ViewGroup.MarginLayoutParams)lp2;
+            txtview2.setText("適当に動いときます");
+            mlp2.setMargins(mlp2.leftMargin + num1, mlp2.topMargin + num2, mlp2.rightMargin + num3, mlp2.bottomMargin + num4);
+            txtview2.setLayoutParams(mlp2);
+        }
+    }
+
+
+
+
+
+    class Button1LongClickListener implements View.OnLongClickListener {
+        @Override
+        public boolean onLongClick(View v) {
+            TextView txtview = (TextView)findViewById(R.id.ID_TXTVIEW);
+            ViewGroup.LayoutParams lp = txtview.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams)lp;
+            txtview.setText("左！");
+            mlp.setMargins(mlp.leftMargin - 120, mlp.topMargin, mlp.rightMargin + 120, mlp.bottomMargin);
+            txtview.setLayoutParams(mlp);
+            return false;
+        }
+    }
+
+    class Button2LongClickListener implements View.OnLongClickListener {
+        @Override
+        public boolean onLongClick(View v) {
+            TextView txtview = (TextView)findViewById(R.id.ID_TXTVIEW);
+            ViewGroup.LayoutParams lp = txtview.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams)lp;
+            txtview.setText("右！");
+            mlp.setMargins(mlp.leftMargin + 120, mlp.topMargin, mlp.rightMargin - 120, mlp.bottomMargin);
+            txtview.setLayoutParams(mlp);
+            return false;
+        }
+    }
+
+    class Button3LongClickListener implements View.OnLongClickListener {
+        @Override
+        public boolean onLongClick(View v) {
+            TextView txtview = (TextView)findViewById(R.id.ID_TXTVIEW);
+            ViewGroup.LayoutParams lp = txtview.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams)lp;
+            txtview.setText("上！");
+            mlp.setMargins(mlp.leftMargin, mlp.topMargin - 120, mlp.rightMargin, mlp.bottomMargin + 120);
+            txtview.setLayoutParams(mlp);
+            return true;
+        }
+    }
+
+    class Button4LongClickListener implements View.OnLongClickListener {
+        @Override
+        public boolean onLongClick(View v) {
+            TextView txtview = (TextView)findViewById(R.id.ID_TXTVIEW);
+            ViewGroup.LayoutParams lp = txtview.getLayoutParams();
+            ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams)lp;
+            txtview.setText("下！");
+            mlp.setMargins(mlp.leftMargin, mlp.topMargin + 120, mlp.rightMargin, mlp.bottomMargin - 120);
+            txtview.setLayoutParams(mlp);
+            return true;
+        }
+    }
+}
